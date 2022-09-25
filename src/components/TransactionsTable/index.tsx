24ad63eaf6378f9541666f763f0d6eeaf6e9e181
jaimesson <leo.jaimesson@gmail.com>
2022-09-25 17:22:50 -0300

    adiciona o axios e o miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,31 +1,40 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
-  return <Container>
-    <table>
-      <thead>
-        <tr>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </tr>
-      </thead>
+  useEffect(() => {
+    api.get("transactions")
+      .then((response) => console.log(response.data));
+  }, []);
 
-      <tbody>
-        <tr>
-          <td>Titulo</td>
-          <td className="deposit">R$100</td>
-          <td>categiria</td>
-          <td>20/02/2020</td>
-        </tr>
-        <tr>
-          <td>Titulo</td>
-          <td className="withdraw">R$100</td>
-          <td>categiria</td>
-          <td>20/02/2020</td>
-        </tr>
-      </tbody>
-    </table>
-  </Container>
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Titulo</td>
+            <td className="deposit">R$100</td>
+            <td>categiria</td>
+            <td>20/02/2020</td>
+          </tr>
+          <tr>
+            <td>Titulo</td>
+            <td className="withdraw">R$100</td>
+            <td>categiria</td>
+            <td>20/02/2020</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
